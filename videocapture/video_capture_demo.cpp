bcd6957684c11dc226fe67440166440e4a027d8d
haska1025 <evilwolf125@163.com>
2019-09-09 09:57:03 +0800

    Add videcapture project

---
+++ videocapture/video_capture_demo.cpp
@@ -0,0 +1,84 @@
+﻿// pch.cpp: 与预编译标头对应的源文件；编译成功所必需的
+
+#include "video_capture_demo.h"
+
+
+#include <modules/video_capture/video_capture.h>
+#include <modules/video_capture/video_capture_factory.h>
+#include <media/engine/webrtcvideocapturerfactory.h>
+
+#include <iostream>
+#include <algorithm>
+
+// 一般情况下，忽略此文件，但如果你使用的是预编译标头，请保留它。
+
+VideoCaptureDemo::VideoCaptureDemo()
+{
+}
+
+VideoCaptureDemo::~VideoCaptureDemo()
+{
+}
+
+
+void VideoCaptureDemo::GetVideoDeviceInfo()
+{
+	std::unique_ptr<webrtc::VideoCaptureModule::DeviceInfo> info(
+		webrtc::VideoCaptureFactory::CreateDeviceInfo());
+	if (!info) {
+		return;
+	}
+	int num_devices = info->NumberOfDevices();
+	for (int i = 0; i < num_devices; ++i) {
+		const uint32_t kSize = 256;
+		char name[kSize] = { 0 };
+		char id[kSize] = { 0 };
+		char product[kSize] = { 0 };
+		if (info->GetDeviceName(i, name, kSize, id, kSize, product, kSize) != -1) {
+			VideoDeviceInfo vdi;
+			vdi.deviceName = name;
+			vdi.deviceId = id;
+			vdi.productId = product;
+
+			std::cout << "Name:" << name << ", id:" << id << ", product:" << product << std::endl;
+			videos_.push_back(vdi);
+		}
+	}
+}
+
+void VideoCaptureDemo::StartCapture()
+{
+	cricket::WebRtcVideoDeviceCapturerFactory factory;
+	std::unique_ptr<cricket::VideoCapturer> capturer;
+	for (const auto& vdi : videos_) {
+		capturer = factory.Create(cricket::Device(vdi.deviceName, 0));
+		if (capturer) {
+			break;
+		}
+	}
+
+	this->capturer_ = std::move(capturer);
+
+	cricket::VideoFormat format;
+
+	format.height = 720;
+	format.width = 1280;
+	format.interval = FPS_TO_INTERVAL(30);
+	format.fourcc = cricket::FOURCC_ANY;
+
+	cricket::VideoFormat best_format;
+
+	capturer_->GetBestCaptureFormat(format, &best_format);
+
+	std::cout << "best format height:" << best_format.height << ", width:" << best_format.width 
+		<< ", interval:" << best_format.interval << ", fourcc:" << best_format.fourcc << std::endl;
+
+	capturer_->Start(best_format);
+	capturer_->AddOrUpdateSink(this, rtc::VideoSinkWants());
+}
+// VideoSinkInterface implementation
+void VideoCaptureDemo::OnFrame(const webrtc::VideoFrame& frame)
+{
+	std::cout << "OnFrame size:" << frame.size() << ", width:" << frame.width() 
+		<< ", height:" << frame.height() << ", timestamp:" << frame.timestamp() << std::endl;
+}
---
+++ videocapture/video_capture_demo.h
@@ -0,0 +1,49 @@
+﻿// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+#ifndef _VIDEO_CAPTURE_H_
+#define _VIDEO_CAPTURE_H_
+
+
+#include <api/videosinkinterface.h>
+#include <api/video/video_frame.h>
+
+#include <media/base/videocapturer.h>
+
+#include <vector>
+#include <memory>
+
+class VideoCaptureDemo : public rtc::VideoSinkInterface<webrtc::VideoFrame>
+{
+public:
+
+	struct VideoDeviceInfo
+	{
+		std::string deviceName;
+		std::string deviceId;
+		std::string productId;
+	};
+
+public:
+	VideoCaptureDemo();
+	~VideoCaptureDemo();
+
+	void GetVideoDeviceInfo();
+
+	void StartCapture();
+	// VideoSinkInterface implementation
+	void OnFrame(const webrtc::VideoFrame& frame) override;
+
+private:
+	std::vector<VideoDeviceInfo> videos_;
+
+	std::unique_ptr<cricket::VideoCapturer> capturer_;
+};
+
+
+#endif //_VIDEO_CAPTURE_H_
